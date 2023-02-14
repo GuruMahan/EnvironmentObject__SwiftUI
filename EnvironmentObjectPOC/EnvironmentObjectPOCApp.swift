e3bd5c256a9ec262e1fe072ffb521f7801e0c0c9
GuruMahan <iosdeve2022@gmail.com>
2023-02-14 11:26:00 +0530

    Initial Commit

---
+++ EnvironmentObjectPOC/EnvironmentObjectPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectPOCApp.swift
+//  EnvironmentObjectPOC
+//
+//  Created by Guru Mahan on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
